b429af9fb04842df4baaccc71cb179ea0ba6ec85
MrAngle <mr.angle69@gmail.com>
2017-12-22 20:23:46 +0100

    Patrze czy dziala

---
+++ Frame.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Kolko
+{
+
+    public class Frame : IFrame
+    {
+        private List<byte> frame_holder = new List<byte>();
+        private ControlElements control_holder = new ControlElements();
+
+        #region Private zone
+        static Random rnd = new Random();
+        static private byte GenerateRandomByte()
+        {
+            return (byte)rnd.Next(0, 2);
+        }
+        static private ulong GetPartInDec(List<byte> nList)
+        {
+            string result = string.Join("", nList);
+            ulong InDec = Convert.ToUInt64(result, 2);
+            return InDec;
+        }
+        private void Show(List<byte> array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+        private void AddElements(List<byte> array, int number)
+        {
+            String binary = Convert.ToString(number, 2);
+            int toList;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (Int32.TryParse(Convert.ToString(binary[i]), out toList)) ;
+                else
+                    Console.WriteLine("String could not be parsed.");
+                array.Add(Convert.ToByte((byte)toList));
+            }
+        }
+        private Frame()
+        {
+
+        }
+        #endregion
+
+        #region Builder zone
+        public class Builder
+        {
+            Frame fra = new Frame();
+            /// <summary>
+            /// Tworzy ramke z losowo wygenerowanymi bitami.
+            /// </summary>
+            /// <param name="size">Okresla dlugosc ramki</param>
+            public Builder RandomFrame(int size)
+            {
+                fra.frame_holder.RemoveAll(item => true == true);
+                fra.CreateRandomInformationPart(size);
+                return this;
+            }
+            /// <summary>
+            /// Tworzy ramke przechowywyjaca podana liczbe jako argument w postaci binarnej. Rozmiar ramki zalezny od podanej liczby
+            /// </summary>
+            /// <param name="number">Liczba okreslajaca informacyjna czesc ramki</param>
+            public Builder SetFrame(int number)
+            {
+                fra.SetInformationPart(number);
+                return this;
+            }
+            /// <summary>
+            /// Wpisuje podana liczbe w postaci binarnej jako czesc kontrolna ramki
+            /// </summary>
+            /// <param name="number">Liczba okreslajaca kontrolna czesc ramki</param>
+            public Builder SetControl(int number)
+            {
+                fra.SetControlPart(number);
+                return this;
+            }
+
+            /// <summary>
+            /// Zwraca ramke wygenerowana przez poprzednie metody
+            /// </summary>
+            public Frame Create()
+            {
+                return fra;
+            }
+        }
+        #endregion
+
+
+        ~Frame()
+        {
+            Console.WriteLine("Kasuje");
+        }
+        public void CreateRandomInformationPart(int size)
+        {
+            
+            for (int i = 0; i < size; i++)
+            {
+                frame_holder.Add(GenerateRandomByte());
+            }
+        }
+
+        #region Show Methods
+        public void ShowInformationPart()
+        {
+            Console.Write("Frame : ");
+            Show(frame_holder);
+        }
+        public void ShowControlPart()
+        {
+            Console.Write("Kontrola : ");
+            Show(control_holder.GetList());
+        }
+        #endregion
+
+        #region Get Methods
+        public List<byte> GetInformationPart()
+        {
+            return frame_holder;
+        }
+        public List<byte> GetControlPart()
+        {
+            return control_holder.GetList();
+        }
+        public ulong GetControlPartInDec()
+        {
+            return GetPartInDec(this.GetControlPart());
+        }
+        public ulong GetInformationPartInDec()
+        {
+            return GetPartInDec(this.GetInformationPart());
+        }
+        #endregion
+
+        #region Set Methods
+        public void SetInformationPart(int number)
+        {
+            frame_holder.RemoveAll(item => true == true);
+            AddElements(control_holder.GetList(), number);
+        }
+        public void SetControlPart(int number)
+        {
+            control_holder.GetList().RemoveAll(item => true == true); //kasuje wszystkie elementy, zawsze
+            AddElements(control_holder.GetList(), number);
+        }
+        #endregion
+
+        public byte this[int number]
+        {
+            get
+            {
+                try
+                {
+                    return frame_holder[number];
+                }
+                catch (Exception)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            set
+            {
+                /* jakby ktos chciał ustawic inna liczbe :))) */
+                if ( value > 1 || value < 0)
+                {
+                    value = 1;
+                } 
+                try
+                {
+                    frame_holder[number] = value;
+                }
+                catch (Exception)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+  
+
+    }
+}
---
+++ Pakiet.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+/*
+ *                  Pakiet
+ *                  [p][p][p][p][p][p]  - jakies dodatkowe miejsce na info
+ *                  [1][1][0][1][0][1]
+ *                  [1][1][0][1][0][1]
+ *                  [1][1][0][1][0][1]
+ *                  [1][1][0][1][0][1]
+ *                  
+ *                  
+ *                  Ramki
+ *                  [1, 1, 1, 1]
+ *                  [1, 1, 1, 1]
+ *                  [0, 0, 0, 0]
+ *                  [1, 1, 1, 1]
+ *                  [0, 0, 0, 0]
+ *                  [1, 1, 1, 1]
+ *                  
+ *                  lub
+ *                  
+ *                  Pakiet
+ *                  [a][b][c][d][e][f]  - wskazniki na ramki lub listy obiektow
+ *                  
+ *                  Ramki
+ *                  a = [1, 1, 1, 1]
+ *                  b = [1, 1, 1, 1]
+ *                  c = [0, 0, 0, 0]
+ *                  d = [1, 1, 1, 1]
+ *                  e = [0, 0, 0, 0]
+ *                  f = [1, 1, 1, 1]
+ *                  
+ *                  
+ *                  lub 
+ *                  
+ *                  Pakiet
+ *                  [1][1][0][1][0][1]
+ *                  [1][1][0][1][0][1]
+ *                  [1][1][0][1][0][1]
+ *                  [1][1][0][1][0][1]
+ * */
+
+
+
+/*
+ *        //Tablica postrzepiona - nie obawiamy sie o wymiar poszczegolnych pakietow
+    void utworz_pakiet()
+    {
+        byte[][] pakiet = new byte[][]
+        {
+        new byte[] { 1, 1, 1, 1, 0, 0, 0, 0 },
+        new byte[] { 1, 1, 1, 1, 0, 0, 0, 0 },
+        new byte[] { 1, 1, 1, 1, 0, 0, 0, 0 },
+        new byte[] { 1, 1, 1, 1, 0, 0, 0, 0 },
+        new byte[] { 1, 1, 1, 1, 0, 0, 0, 0 }
+        };
+ * 
+ **/
+
+
+
+namespace Projekt_Kolko
+{
+
+
+    public class Pakiet : IPakiet
+    {
+        private List<Frame> FrameList = new List<Frame>();
+        private ControlElements control_holder = new ControlElements();
+
+        #region Private_zone<3
+        private void Show(List<byte> array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+        private void AddElements(List<byte> array, int number)
+        {
+            String binary = Convert.ToString(number, 2);
+            int toList;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (Int32.TryParse(Convert.ToString(binary[i]), out toList)) ;
+                else
+                    Console.WriteLine("String could not be parsed.");
+                array.Add(Convert.ToByte((byte)toList));
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Dodaje ramke na koniec listy
+        /// </summary>
+        public void AddFrame(Frame item)
+        {
+            try
+            {
+                FrameList.Add(item);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Blad przy dodawaniu Ramki do Pakietu!");
+            }
+        }
+        /// <summary>
+        /// Usuwa WSZYSTKIE ramki.
+        /// </summary>
+        public void DeleteFrames()
+        {
+            FrameList.Clear();
+        }
+
+        /// <summary>
+        /// Tworzy liste ramek(bez czesci kontrolnej). Sposob tworzenia zalezny od argumentow
+        /// </summary>
+        /// <param name="numberOfFrames">Liczba ramek do stworzenia</param>
+        /// <param name="size">Dlugosc kazdej ramki ( bez czesci kontrolnej )</param>
+        /// <param name="random">Okresla czy zawartosc ramek powinna byc losowa "TO DO" FALSE JESZCZE NIE DZIALA!!!!</param>
+        public void GenerateFrameList(int numberOfFrames, int size, bool random = true)
+        {
+            DeleteFrames();
+            for (int i = 0; i < numberOfFrames; i++)
+            {
+                AddFrame(new Frame.Builder().RandomFrame(size).SetControl(7).Create());
+            }
+        }
+
+        /// <summary>
+        /// Ustawia czesc kontrolna zamieniajac liczbę z systemu 10 na 2. Dlugosc listy zawierajacej 
+        /// kontrolna czesc zalezna jest od zajmowanych bitow przez argument.
+        /// </summary>
+        /// <param name="number">Liczba w systemie dziesietnym</param>
+        public void SetControlPart(int number)
+        {
+            control_holder.GetList().RemoveAll(item => true == true); //kasuje wszystkie elementy, zawsze
+            AddElements(control_holder.GetList(), number);
+        }
+
+
+
+        #region Get Methods
+        /// <summary>
+        /// Zwraca liste zawierajaca bity kontroli
+        /// </summary>
+        /// <returns></returns>
+        public List<byte> GetControlPart()
+        {
+            return control_holder.GetList();
+        }
+        /// <summary>
+        /// Zwraca liste ramek
+        /// </summary>
+        /// <returns></returns>
+        public List<Frame> GetFrames()
+        {
+            return FrameList;
+        }
+        #endregion
+
+        #region Show Function
+        /// <summary>
+        /// Wyświetla czesc kontrolna w postaci binarnej
+        /// </summary>
+        /// <returns></returns>
+        public void ShowControlPart()
+        {
+            Console.Write("Kontrola : ");
+            Show(control_holder.GetList());
+        }
+        /// <summary>
+        /// Wyświetla wszystkie ramki w postaci binarnej
+        /// </summary>
+        /// <returns></returns>
+        public void ShowFrames()
+        {
+            foreach (var item in FrameList)
+            {
+                item.ShowInformationPart();
+                item.ShowControlPart();
+            }
+        }
+        #endregion
+
+
+        public Frame this[int number]
+        {
+            get
+            {
+                try
+                {
+                    return FrameList[number];
+                }
+                catch (Exception)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+    }
+
+
+
+
+    
+}
