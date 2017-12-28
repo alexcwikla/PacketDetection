59ed22606f7e5c943ad95ffb338177f2f6a3c927
alexcwikla <213axl@gmail.com>
2017-12-28 17:15:25 +0100

    Update ParityBitControl.cs
    ParityBitCheck

--- ControlType/ParityBitControl.cs
+++ ControlType/ParityBitControl.cs
@@ -33,6 +33,50 @@ namespace Projekt_Kolko
             return parityBit;
         }
 
+        public byte CollisionDetection(Frame nFrame)
+        {
+            ulong count = (ulong)nFrame.GetInformationPart().Count(b => b == 1) + nFrame.GetControlPart().GetControlPartInDec(); //Zlicza jedynki z infoParty i controlPart
+            Console.WriteLine("Count - " + count);  //Do wykasowania linijka
+
+            if (count % 2 == 0)                     // Sprawdzanie bledu
+            {
+                Console.WriteLine("Wyglada na to ze jest ok");
+                return 0;                           // Blad nie wystapil
+            }                                       // TODO: Nie wykryto a blad wystapil 
+
+            else
+            {
+                Console.WriteLine("Blad!");
+                return 1;                           // Blad wystapil
+            }                                    // TODO: Bledne wykrycie
+        }
+
+        public byte CollisionDetection(Package nPakiet)
+        {
+            ulong count = nPakiet.GetControlPart().GetControlPartInDec();
+
+            Console.WriteLine("Count przed dodaniem ramek: " + count);          // Do usuniecia!
+
+            foreach (var frame in nPakiet.GetFrames())                          //Zlicza jedynki z infoParty calego pakietu
+            {
+                count += (ulong)frame.GetInformationPart().Count(b => b == 1);  //Zlicza jedynki z infoParty  z pojedynczej ramki
+            }
+
+            Console.WriteLine("Count - " + count);  //Do wykasowania linijka
+
+            if (count % 2 == 0)                     // Sprawdzanie bledu
+            {
+                Console.WriteLine("Wyglada na to ze jest ok");
+                return 0;                           // Blad nie wystapil
+            }                                       // TODO: Nie wykryto a blad wystapil 
+
+            else
+            {
+                Console.WriteLine("Blad!");
+                return 1;                           // Blad wystapil
+            }
+        }
+
 
     }
 }
